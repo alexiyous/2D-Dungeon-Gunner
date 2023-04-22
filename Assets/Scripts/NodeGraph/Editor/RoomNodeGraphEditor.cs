ea1f2da96b0de351b8c9d079c8d4b5592be1b443
alexiyous <alexiusrenanta@gmail.com>
2023-04-23 01:37:21 +0700

    Drawable RoomNode in RoomNodeGraphEditor

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
 
 public class RoomNodeGraphEditor : EditorWindow
 {
     private GUIStyle roomNodeStyle;
+    private static RoomNodeGraphSO currentRoomNodeGraph;
+    private RoomNodeTypeListSO roomNodeTypeList;
 
     // Node Layout Values
     private const float nodeWidth = 160f;
@@ -15,7 +16,7 @@ public class RoomNodeGraphEditor : EditorWindow
 
     // Create a new Menu Item Called "Room Node Graph Editor" Just like Hierarcy window but blank. We could access it by clicking
     // Window>Dungeun Editor>Room Node Graph Editor
-    [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeun Editor/Room Node Graph Editor")]
+    [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
 
     // Function to open the menu item that we created
     private static void OpenWindow()
@@ -31,21 +32,126 @@ public class RoomNodeGraphEditor : EditorWindow
         roomNodeStyle.normal.textColor = Color.white;
         roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
         roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+
+        // Load Room node Types
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
     }
+
+    // Open the room node graph editor window if a room node graph scriptable object asset is double clicked in the inspector
+    [OnOpenAsset(0)] // Need the namespace UnityEditor.Callbacks
+    public static bool OnDoubleClickAsset(int instanceID, int line)
+    {
+        // if the SO that been double clicked is a RoomNodeGraphSO, the the roomNodeGraph will be populated by a value, it is not then it will be NULL
+        RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+
+        // Check wether the roomNodeGraph is null or not
+        if (roomNodeGraph != null)
+        {
+            OpenWindow();
+
+            currentRoomNodeGraph = roomNodeGraph;
+
+            return true;
+        }
+        return false;
+    }
+
+    
 
     // Draw Editor GUI
     private void OnGUI()
     {
-        // Create a node within a certain defined position
-        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-        // Display Label
-        EditorGUILayout.LabelField("Node 1");
-        GUILayout.EndArea();
+        // If a scriptable object of type RoomNodeGraphSO has been selected then process
+        if (currentRoomNodeGraph != null)
+        {
+            // Process Events
+            ProcessEvents(Event.current);
 
-        // Create a node within a certain defined position
-        GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-        // Display Label
-        EditorGUILayout.LabelField("Node 2");
-        GUILayout.EndArea();
+            // Draw Room Nodes
+            DrawRoomNodes();
+        }
+
+        if (GUI.changed)
+        {
+            Repaint();
+        }
+    }
+
+    private void ProcessEvents(Event currentEvent)
+    {
+        ProcessRoomNodeGraphEvents(currentEvent);
+    }
+
+    // Process Room Node Graph Events
+    private void ProcessRoomNodeGraphEvents(Event currentEvent)
+    {
+        switch (currentEvent.type)
+        {
+            // Process Mouse Down Events
+            case EventType.MouseDown:
+                ProcessMouseDownEvent(currentEvent);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // Process mouse down events on the room node graph (not over a node)
+    private void ProcessMouseDownEvent(Event currentEvent)
+    {
+        // Process right click mouse down on graph event (show context menu)
+        if (currentEvent.button == 1)
+        {
+            ShowContextMenu(currentEvent.mousePosition);
+        }
+    }
+
+    // Show the context menu
+    private void ShowContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu menu = new GenericMenu();
+
+        menu.AddItem(new GUIContent("Create Room Node"), false, CreateRoomNode, mousePosition);
+
+        menu.ShowAsContext();
+    }
+
+    // Create a room node at the mouse position
+    private void CreateRoomNode(object mousePositionObject)
+    {
+        CreateRoomNode(mousePositionObject, roomNodeTypeList.list.Find(x => x.isNone));
+    }
+
+    // Create a room node at the mouse position - overloaded to also pass in RoomNodeType
+    private void CreateRoomNode(object mousePositionObject, RoomNodeTypeSO roomNodeType)
+    {
+        Vector2 mousePosition = (Vector2)mousePositionObject;
+
+        // Create room node scriptable object asset
+        RoomNodeSO roomNode = ScriptableObject.CreateInstance<RoomNodeSO>();
+
+        // add room node to current room node graph room node list
+        currentRoomNodeGraph.roomNodeList.Add(roomNode);
+
+        // set room node values
+        roomNode.Initialize(new Rect(mousePosition, new Vector2(nodeWidth, nodeHeight)), currentRoomNodeGraph, roomNodeType);
+
+        // add room node to room node graph scriptable object asset database
+        AssetDatabase.AddObjectToAsset(roomNode, currentRoomNodeGraph);
+
+        AssetDatabase.SaveAssets();
+    }
+
+    // Draw room nodes in the graph window
+    private void DrawRoomNodes()
+    {
+        // loop through all room nodes and draw them
+        foreach (RoomNodeSO roomNode in currentRoomNodeGraph.roomNodeList)
+        {
+            roomNode.Draw(roomNodeStyle);
+        }
+
+        GUI.changed = true;
     }
 }
---
+++ Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Create a new asset menu (Could be accessed by clicking the assets tab on top) with the difined path (menuName)
+[CreateAssetMenu(fileName = "RoomNodeGraph", menuName = "Scriptable Objects/Dungeon/Room Node Graph")]
+public class RoomNodeGraphSO : ScriptableObject
+{
+    [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+    [HideInInspector] public List<RoomNodeSO> roomNodeList = new List<RoomNodeSO>();
+    [HideInInspector] public Dictionary<string, RoomNodeSO> roomNodeDictionary = new Dictionary<string, RoomNodeSO>();
+}
